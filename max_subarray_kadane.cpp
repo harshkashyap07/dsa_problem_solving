47c0cb8139c43d7d317d60366aad5d06e4732050
Harsh Kashyap <kashyap007harsh@gmail.com>
2026-04-17 12:48:40 +0530

    Second Commit

---
+++ max_subarray_kadane.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<climits>
+#include<vector>
+using namespace std;
+int max_subarray(vector<int>nums)
+{
+	int currsum=0;
+	int max_sum=INT_MIN;
+	for(int i:nums)
+	{
+		currsum+=i;
+		max_sum=max(currsum,max_sum);
+		if(currsum<0)
+		{
+			currsum=0;
+		}
+	}
+	return max_sum;
+}
+int main()
+{
+	int n;
+	cin>>n;
+	vector<int>nums(n);
+	for(int &i:nums)
+	{
+		cin>>i;
+	}
+	cout<<max_subarray(nums);
+	return 0;
+}
